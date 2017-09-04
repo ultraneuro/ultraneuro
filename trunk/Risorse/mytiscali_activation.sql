1d9af3558b306f93d00ad28d0986160c9ea15a68
fdalmonte <fdalmonte@4f122cde-7998-4887-bbb2-2540e0ee65aa>
2017-09-04 15:06:41 +0000

    git-svn-id: svn://svn.dev.tiscali.sys/SIT@51 4f122cde-7998-4887-bbb2-2540e0ee65aa

--- trunk/Risorse/mytiscali_activation.sql
+++ trunk/Risorse/mytiscali_activation.sql
@@ -1,10 +1,17 @@
+/*Query per: 
+-	Attivare la email su mytiscali
+-	Recuperare il PIN di un dato cliente
+*/
+
+
+/*Recuperare le email esistenti per evitare collisioni*/
 select t.*, t.rowid from TIS_MYTISCALI_AUTH t
 where t.email like '%rlah%'
 --for update
 order by 1 desc
 
 
--- PIN
+-- Recupero del PIN da settare nella maschera di registrazione
 select attrib_46 AS PIN, row_id, created, created_by, last_upd, last_upd_by, attrib_45 from siebel.s_org_ext_x 
 where row_id in (select a.row_id from siebel.s_org_ext a
 where a.ou_num = '400467342'
